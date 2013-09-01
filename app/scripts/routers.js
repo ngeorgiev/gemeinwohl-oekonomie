f06882f09b61511ee41707c6bcc609926cc7c431
Nikolay Georgiev <nikolay@georgiev.cc>
2013-09-01 15:46:03 +0200

    fixed back button to main page

--- app/scripts/routers.js
+++ app/scripts/routers.js
@@ -2,20 +2,20 @@
 
 var Router = {
 
-    hashSymbol : '#',
-    matrixPrefix : 'matrix-',
+    hashSymbol: '#',
+    matrixPrefix: 'matrix-',
 
     /**
      * Initializes the URL Routing
      */
-    init : function () {
+    init: function () {
 
         // Open the given hash value from the URL
         var urlHash = window.location.hash;
         if (urlHash && urlHash.length > 1) {
             var hash = urlHash.substr(1);
 
-            if(hash.startsWith(Router.matrixPrefix)) {
+            if (hash.startsWith(Router.matrixPrefix)) {
                 // hack: assumes the format "matrix-<pageid>"
                 if (hash.length === 9) {
                     Router.showPage(hash);
@@ -26,9 +26,17 @@ var Router = {
                 }
             }
         }
+
+        // if the user clicks the back button to go the main page, show the matrix
+        $(window).bind('hashchange', function () {
+            var urlHash = window.location.hash;
+            if(!urlHash) {
+                Router.showMatrix();
+            }
+        });
     },
 
-    showPage : function (indicatorId, indicatorDetailId) {
+    showPage: function (indicatorId, indicatorDetailId) {
 
         // init params
         if (indicatorId.startsWith(Router.matrixPrefix)) { // if matrix URL
@@ -47,7 +55,7 @@ var Router = {
         window.location.hash = Router.hashSymbol + indicatorId + indicatorDetailId;
     },
 
-    showMatrix : function () {
+    showMatrix: function () {
         $('#' + currentIndicatorId).fadeOut(100, function () {
             $('#gwoe-matrix').fadeIn(300);
         });
