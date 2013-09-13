5b9bfadff4b37197287237394333170928ca9cf6
Nikolay Georgiev <nikolay.h.georgiev@gmail.com>
2013-09-13 04:47:07 +0200

    created dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -395,6 +395,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', [
         'clean:dist',
+        'filesToJson',
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
