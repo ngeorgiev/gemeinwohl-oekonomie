e56fceb4c2c0714dac1ded6fd040c6ee8149f2b0
Nikolay Georgiev <nikolay.h.georgiev@gmail.com>
2013-09-15 14:36:30 +0200

    added real push

--- push-to-github-pages.sh
+++ push-to-github-pages.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # Updates the GitHub page with the contents in the dist folder
-# find ../gh-pages/gemeinwohl-oekonomie -name "*" -type f -not -path "*.git*" | xargs rm
+find ../gh-pages/gemeinwohl-oekonomie -name "*" -type f -not -path "*.git*" | xargs rm
 cp -r dist/* ../gh-pages/gemeinwohl-oekonomie
+cd ../gh-pages/gemeinwohl-oekonomie
+git add *
+git commit -a -m "latest commit"
+git push origin gh-pages
